0f8e096269feafc9eefa58559a1565bd43532217
mrssmith <marisolbsmith@yahoo.com>
2021-10-20 22:10:42 -0500

    heroku

--- Develop/server.js
+++ Develop/server.js
@@ -27,4 +27,56 @@ app.get("/api/notes", function (req, res) {
     res.json(notes);
   });
 });
+// Starting server
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+// Create a new note
+app.post("/api/notes", function (req, res) {
+  // Using a random letter and current date/time to create an id for each note
+  let randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  let id = randomLetter + Date.now();
+  let newNote = {
+    id: id,
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(typeof notes);
+  notes.push(newNote);
+  const stringifyNote = JSON.stringify(notes);
+  res.json(notes);
+  fs.writeFile("db/db.json", stringifyNote, (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Note saved to db.json");
+    }
+  });
+});
+
+// Delete note
+app.delete("/api/notes/:id", function (req, res) {
+  let noteID = req.params.id;
+  fs.readFile("db/db.json", "utf8", function (err, data) {
+    let updatedNotes = JSON.parse(data).filter((note) => {
+      console.log("note.id", note.id);
+      console.log("noteID", noteID);
+      return note.id !== noteID;
+    });
+    notes = updatedNotes;
+    const stringifyNote = JSON.stringify(updatedNotes);
+    fs.writeFile("db/db.json", stringifyNote, (err) => {
+      if (err) console.log(err);
+      else {
+        console.log("Note successfully deleted from db.json");
+      }
+    });
+    res.json(stringifyNote);
+  });
+});
+
+// Catch all error route
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
